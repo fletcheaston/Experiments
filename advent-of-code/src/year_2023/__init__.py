185bd9da292d51fd424733e1a2310c8f3bb82db1
Fletcher Easton <fletcheaston@gmail.com>
2023-12-10 23:28:23 -0800

    🎉 AoC 2023 day 11 complete

--- advent-of-code/src/year_2023/__init__.py
+++ advent-of-code/src/year_2023/__init__.py
@@ -1,6 +1,18 @@
 from fastapi import APIRouter
 
-from . import day_1, day_2, day_3, day_4, day_5, day_6, day_7, day_8, day_9, day_10
+from . import (
+    day_1,
+    day_2,
+    day_3,
+    day_4,
+    day_5,
+    day_6,
+    day_7,
+    day_8,
+    day_9,
+    day_10,
+    day_11,
+)
 
 router = APIRouter()
 
@@ -14,3 +26,4 @@ router.include_router(day_7.router, prefix="/day-7")
 router.include_router(day_8.router, prefix="/day-8")
 router.include_router(day_9.router, prefix="/day-9")
 router.include_router(day_10.router, prefix="/day-10")
+router.include_router(day_11.router, prefix="/day-11")
---
+++ advent-of-code/src/year_2023/day_11.py
@@ -0,0 +1,116 @@
+from dataclasses import dataclass, field
+from itertools import combinations
+
+from fastapi import APIRouter, Body
+
+router = APIRouter(tags=["2023 - Day 11: Title"])
+
+
+DOCUMENT_EXAMPLE = []
+
+
+@dataclass
+class Grid:
+    space: list[list[str]]
+    displacement: int
+    galaxies: list[tuple[int, int]] = field(default_factory=list)
+    galaxy_combos: list[tuple[tuple[int, int], tuple[int, int]]] = field(
+        default_factory=list
+    )
+
+    rows_displaced: list[int] = field(default_factory=list)
+    columns_displaced: list[int] = field(default_factory=list)
+
+    def expand(self) -> None:
+        # Expand the rows
+        for index, row in enumerate(self.space):
+            if all([char == "." for char in row]):
+                self.rows_displaced.append(index)
+
+        # Expand the columns
+        for x_index in range(len(self.space[0])):
+            if all(
+                [
+                    self.space[y_index][x_index] == "."
+                    for y_index in range(len(self.space))
+                ]
+            ):
+                self.columns_displaced.append(x_index)
+
+    def pair_galaxies(self) -> None:
+        for y_index, row in enumerate(self.space):
+            for x_index, character in enumerate(row):
+                if character == "#":
+                    self.galaxies.append((x_index, y_index))
+
+        for galaxy_combo in combinations(self.galaxies, 2):
+            self.galaxy_combos.append(galaxy_combo)
+
+    def calculate_galaxy_combo_distances(self) -> int:
+        total_distance = 0
+
+        for galaxy_a, galaxy_b in self.galaxy_combos:
+            x_displacements = 0
+            y_displacements = 0
+
+            for column in self.columns_displaced:
+                if (
+                    galaxy_a[0] > column > galaxy_b[0]
+                    or galaxy_a[0] < column < galaxy_b[0]
+                ):
+                    x_displacements += self.displacement - 1
+
+            for row in self.rows_displaced:
+                if galaxy_a[1] > row > galaxy_b[1] or galaxy_a[1] < row < galaxy_b[1]:
+                    y_displacements += self.displacement - 1
+
+            distance = (
+                abs(galaxy_a[0] - galaxy_b[0])
+                + x_displacements
+                + abs(galaxy_a[1] - galaxy_b[1])
+                + y_displacements
+            )
+
+            total_distance += distance
+
+        return total_distance
+
+
+@router.post("/part-1")
+async def year_2023_day_11_part_1(
+    document: list[str] = Body(
+        ...,
+        embed=True,
+        examples=[DOCUMENT_EXAMPLE],
+    ),
+) -> int:
+    grid = Grid(
+        space=[list(line) for line in document],
+        displacement=2,
+    )
+
+    grid.expand()
+
+    grid.pair_galaxies()
+
+    return grid.calculate_galaxy_combo_distances()
+
+
+@router.post("/part-2")
+async def year_2023_day_11_part_2(
+    document: list[str] = Body(
+        ...,
+        embed=True,
+        examples=[DOCUMENT_EXAMPLE],
+    ),
+) -> int:
+    grid = Grid(
+        space=[list(line) for line in document],
+        displacement=1000000,
+    )
+
+    grid.expand()
+
+    grid.pair_galaxies()
+
+    return grid.calculate_galaxy_combo_distances()

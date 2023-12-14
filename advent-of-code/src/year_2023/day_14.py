fc6237bd6354de90ef1fef95aa27ec9cccbc7a2a
Fletcher Easton <fletcheaston@gmail.com>
2023-12-14 00:21:03 -0800

    🎉 AoC 2023 day 14 part 2 complete

--- advent-of-code/src/year_2023/day_14.py
+++ advent-of-code/src/year_2023/day_14.py
@@ -8,7 +8,7 @@ router = APIRouter(tags=["2023 - Day 14: Title"])
 DOCUMENT_EXAMPLE = []
 
 
-def tilt(line: str) -> str:
+def tilt_left(line: str) -> str:
     rock_map: Counter[int, int] = Counter({0: 0})
     square_rock_map: list[int] = [-1]
     current_index = 0
@@ -50,13 +50,12 @@ async def year_2023_day_14_part_1(
     line_size = len(document[0])
     lines: list[str] = ["" for _ in range(line_size)]
 
-    # Iterate over lines
     for line in document:
         for index, character in enumerate(line):
             lines[index] += character
 
     for index, line in enumerate(lines):
-        lines[index] = tilt(line)
+        lines[index] = tilt_left(line)
 
     for row_index in range(line_size):
         line_total = 0
@@ -71,6 +70,36 @@ async def year_2023_day_14_part_1(
     return total
 
 
+def transpose_90(raw_lines: list[str]) -> list[str]:
+    # Rotates 90 degrees clockwise
+    line_size = len(raw_lines[0])
+    lines: list[str] = ["" for _ in range(line_size)]
+
+    for line in raw_lines:
+        for index, character in enumerate(reversed(line)):
+            lines[index] += character
+
+    return lines
+
+
+def transpose_180(raw_lines: list[str]) -> list[str]:
+    new_lines = raw_lines.copy()
+
+    for _ in range(2):
+        new_lines = transpose_90(new_lines)
+
+    return new_lines
+
+
+def transpose_270(raw_lines: list[str]) -> list[str]:
+    new_lines = raw_lines.copy()
+
+    for _ in range(3):
+        new_lines = transpose_90(new_lines)
+
+    return new_lines
+
+
 @router.post("/part-2")
 async def year_2023_day_14_part_2(
     document: list[str] = Body(
@@ -79,10 +108,46 @@ async def year_2023_day_14_part_2(
         examples=[DOCUMENT_EXAMPLE],
     ),
 ) -> int:
+    size = 1_000_000_000
+
+    counter = 0
+    cycle_repeats: int | None = None
+    cycle_counter: dict[str, int] = {}
+    cycles: dict[int, list[str]] = {}
+
+    while counter < size:
+        north = [tilt_left(line) for line in transpose_90(document)]
+        west = [tilt_left(line) for line in transpose_270(north)]
+        south = [tilt_left(line) for line in transpose_270(west)]
+        east = [tilt_left(line) for line in transpose_270(south)]
+
+        cycled = transpose_180(east)
+
+        cycles[counter] = cycled
+
+        cycle_key = "".join(cycled)
+
+        if cycle_key in cycle_counter:
+            cycle_repeats = cycle_counter[cycle_key]
+            break
+
+        else:
+            cycle_counter[cycle_key] = counter
+
+        counter += 1
+        document = cycled
+
+    if cycle_repeats is None:
+        last_cycle = cycles[counter - 1]
+    else:
+        final_cycle = (
+            cycle_repeats + (size - cycle_repeats) % (counter - cycle_repeats) - 1
+        )
+        last_cycle = cycles[final_cycle]
+
     total = 0
 
-    # Iterate over lines
-    for line in document:
-        pass
+    for index, line in enumerate(last_cycle):
+        total += line.count("O") * (len(last_cycle) - index)
 
     return total
--- advent-of-code/tests/year_2023/day_14/test.py
+++ advent-of-code/tests/year_2023/day_14/test.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import pytest
 from fastapi.testclient import TestClient
 
-from src.year_2023.day_14 import tilt
+from src.year_2023.day_14 import tilt_left, transpose_90
 
 
 @pytest.mark.parametrize(
@@ -17,8 +17,8 @@ from src.year_2023.day_14 import tilt
         ("O.O#..OO.#.O", "OO.#OO...#O."),
     ],
 )
-def test_tilt(line: str, output: str) -> None:
-    assert tilt(line) == output
+def test_tilt_left(line: str, output: str) -> None:
+    assert tilt_left(line) == output
 
 
 @pytest.mark.parametrize(
@@ -33,7 +33,6 @@ def test_part_1(
     output: int,
     test_client: TestClient,
 ) -> None:
-    print()
     with open(Path(__file__).with_name(filename), "r") as file:
         response = test_client.post(
             "2023/day-14/part-1",
@@ -47,10 +46,23 @@ def test_part_1(
 
 
 @pytest.mark.parametrize(
+    "lines,output",
+    [
+        (["ABC", "DEF", "GHI"], ["CFI", "BEH", "ADG"]),
+        (["CFI", "BEH", "ADG"], ["IHG", "FED", "CBA"]),
+        (["IHG", "FED", "CBA"], ["GDA", "HEB", "IFC"]),
+        (["GDA", "HEB", "IFC"], ["ABC", "DEF", "GHI"]),
+    ],
+)
+def test_transpose(lines: list[str], output: list[str]) -> None:
+    assert transpose_90(lines) == output
+
+
+@pytest.mark.parametrize(
     "filename,output",
     [
-        ("example.txt", 0),
-        # ("input.txt", 0),
+        ("example.txt", 64),
+        ("input.txt", 90551),
     ],
 )
 def test_part_2(
